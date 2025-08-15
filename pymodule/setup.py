e9ad8a639c569e8d3fda33eb55542f1f7ef28e20
Mingjie Jian <jian-mingjie@outlook.com>
2025-08-15 19:56:56 +0900

    (make) modify for macos-13/15 - 5

--- pymodule/setup.py
+++ pymodule/setup.py
@@ -1,9 +1,9 @@
 from os.path import join, dirname, abspath
 from distutils.core import setup, Extension
-import numpy.distutils.misc_util
+import numpy as np
 
 libdir = abspath(join(dirname(__file__), "../lib"))
-include_dirs = numpy.distutils.misc_util.get_numpy_include_dirs()
+include_dirs = np.get_include()
 include_dirs += [libdir]
 
 module = Extension(
